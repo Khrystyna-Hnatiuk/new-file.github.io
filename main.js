b4449fc44b1b5a6d05401557f4cc8be19d9e3ff2
Khrystyna-Hnatiuk <114655529+Khrystyna-Hnatiuk@users.noreply.github.com>
2023-07-17 12:38:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function ViewMenu(){
+
+let n = document.getElementById('new');
+n.style.display='block';
+
+
+}
+
+
+function openlogo(){
+     document.getElementById('openLogo').style.display='block';
+    // m.style.display='block';
+}
+
+function ViewMenuNew(){
+document.getElementById('newMenu').style.display='block'
+}
+
+
+function openLogoNew(){
+document.getElementById('openLogoMenu').style.display='block'
+}
+
+function openlogoCont(){
+    document.getElementById('openLogoMenu').style.display='block'
+}
+
+function ViewMenuCont(){
+    document.getElementById('newMenuContact').style.display='block'
+
+}
+
+
+
+function openlogoAbout(){
+    document.getElementById('openLogoAbout').style.display='block'
+
+}
+
+function ViewMenuAbout(){
+    document.getElementById('newMenuAbout').style.display='block'
+    
+}
+
+function ViewMenuBlogs(){
+    document.getElementById('newMenuBlogs').style.display='block'
+    
+}
+
+function openLogoBlogs(){
+    document.getElementById('openlogoBlog').style.display='block'
+    
+}
+
